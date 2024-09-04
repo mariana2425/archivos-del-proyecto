619daac8489beed958fc5fae27393b796833ad53
mariana2425 <est.luz.gamboa.mena@carlosvieco.edu.co>
2024-09-04 13:22:31 -0500

    Add files via upload

---
+++ base.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Usuario(
+   id_usuario integer primary key,
+  nombre text,
+  edad integer,
+  correo text,
+  contrasena integer
+);
+
+
+CREATE TABLE Actividades(
+  id_actividades integer primary key ,
+  actividad text,
+  puntos integer
+);
+
+
+CREATE TABLE Usuario_actividades(
+  "id_usuario" integer,
+  "id_actividades" integer,
+  FOREIGN KEY ("id_usuario") REFERENCES "usuario" ("id"),
+  FOREIGN KEY ("id_actividades") REFERENCES "actividades" ("id")
+  );
+
